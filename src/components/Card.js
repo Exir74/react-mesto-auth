4ba50e700b41dcede79db6e0611d0d97d7355975
Exir74 <exir74@gmail.com>
2023-05-04 09:50:45 +0500

    bugfix

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,10 @@
 import React from "react";
 
 
-function Card({cards, onCardClick}) {
-
-  return cards.map((card) => {
-    function handleClick() {
-      onCardClick(card)
-    }
-
+function Card({card, onCardClick}) {
+  function handleClick() {
+    onCardClick(card)
+  }
     return (
       <div key={card._id} className="cards__item card">
         <button type="button" className="card__trash hover"></button>
@@ -22,7 +19,27 @@ function Card({cards, onCardClick}) {
           </div>
         </div>
       </div>)
-  })
+
+  // return cards.map((card) => {
+  //   function handleClick() {
+  //     onCardClick(card)
+  //   }
+  //
+  //   return (
+  //     <div key={card._id} className="cards__item card">
+  //       <button type="button" className="card__trash hover"></button>
+  //       <button type="button" onClick={handleClick} className="card__button">
+  //         <img className="card__image" src={card.link} alt={card.name}/>
+  //       </button>
+  //       <div className="card__footer">
+  //         <h2 className="card__caption">{card.name}</h2>
+  //         <div className="card__like-wrapper">
+  //           <button type="button" className="card__like"></button>
+  //           <p className="card__like-quantity">{card.likes.length}</p>
+  //         </div>
+  //       </div>
+  //     </div>)
+  // })
 }
 
 export default Card
