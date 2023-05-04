d3d5dde4104873db012d49f4b73035c001386416
Exir74 <exir74@gmail.com>
2023-05-04 09:58:09 +0500

    bugfix1

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,20 +5,21 @@ function Card({card, onCardClick}) {
   function handleClick() {
     onCardClick(card)
   }
-    return (
-      <div key={card._id} className="cards__item card">
-        <button type="button" className="card__trash hover"></button>
-        <button type="button" onClick={handleClick} className="card__button">
-          <img className="card__image" src={card.link} alt={card.name}/>
-        </button>
-        <div className="card__footer">
-          <h2 className="card__caption">{card.name}</h2>
-          <div className="card__like-wrapper">
-            <button type="button" className="card__like"></button>
-            <p className="card__like-quantity">{card.likes.length}</p>
-          </div>
+
+  return (
+    <div key={card._id} className="cards__item card">
+      <button type="button" className="card__trash hover"></button>
+      <button type="button" onClick={handleClick} className="card__button">
+        <img className="card__image" src={card.link} alt={card.name}/>
+      </button>
+      <div className="card__footer">
+        <h2 className="card__caption">{card.name}</h2>
+        <div className="card__like-wrapper">
+          <button type="button" className="card__like"></button>
+          <p className="card__like-quantity">{card.likes.length}</p>
         </div>
-      </div>)
+      </div>
+    </div>)
 
 }
 
