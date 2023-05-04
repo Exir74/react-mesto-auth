b90655346b753d94d41e0ebcfea1a4a9470f49e0
Exir74 <exir74@gmail.com>
2023-05-04 09:54:31 +0500

    bugfix

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,26 +20,6 @@ function Card({card, onCardClick}) {
         </div>
       </div>)
 
-  // return cards.map((card) => {
-  //   function handleClick() {
-  //     onCardClick(card)
-  //   }
-  //
-  //   return (
-  //     <div key={card._id} className="cards__item card">
-  //       <button type="button" className="card__trash hover"></button>
-  //       <button type="button" onClick={handleClick} className="card__button">
-  //         <img className="card__image" src={card.link} alt={card.name}/>
-  //       </button>
-  //       <div className="card__footer">
-  //         <h2 className="card__caption">{card.name}</h2>
-  //         <div className="card__like-wrapper">
-  //           <button type="button" className="card__like"></button>
-  //           <p className="card__like-quantity">{card.likes.length}</p>
-  //         </div>
-  //       </div>
-  //     </div>)
-  // })
 }
 
 export default Card
